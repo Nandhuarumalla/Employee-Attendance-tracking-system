e2ffa165083fc6b76b9472f853fbcc960e5ef54b
Nandhuarumalla <arumallanandini32@gmail.com>
2025-11-30 08:19:24 +0530

    store.js

---
+++ frontend/src/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import attendanceReducer from './attendanceSlice';
+import dashboardReducer from './dashboardSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    attendance: attendanceReducer,
+    dashboard: dashboardReducer,
+  },
+});
+
+export default store;
